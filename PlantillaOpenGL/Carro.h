896e3f6b4107c313afbaa88f46fa9c246f876995
Pablo Eduardo Beltran A <pablo_eduardo00@hotmail.com>
2019-05-02 09:45:36 -0700

    movimiento carro

--- PlantillaOpenGL/Carro.h
+++ PlantillaOpenGL/Carro.h
@@ -4,5 +4,13 @@
 #include "glm/glm.hpp"
 
 class Carro : public Modelo {
-public:Carro();
+public:
+	Carro();
+	void avanzar();
+	void rotar();
+
+private:
+	void actualizarMatrizModelo();
+	vec3 coordenadas = vec3(0.0f, 0.0f, 0.0f);
+
 };
